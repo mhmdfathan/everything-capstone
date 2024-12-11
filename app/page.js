6c6c693de6cf2eb5fb420a2fe329d878e6568c3b
Fathan <102466798+mhmdfathan@users.noreply.github.com>
2024-12-11 10:15:32 +0700

    init

---
+++ app/page.js
@@ -0,0 +1,68 @@
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+    const links = [
+        {
+            title: 'December Backlog',
+            url: 'https://app.clickup.com/9018728086/v/li/901804594450',
+        },
+        {
+            title: 'Github Repo',
+            url: 'https://github.com/gelaws-hub/mjtek-site',
+        },
+        {
+            title: 'One Drive Dokumen Capstone',
+            url: 'https://undipmail-my.sharepoint.com/:f:/g/personal/ibnufadhil12_students_undip_ac_id/EvKtMN1qWT1NhLduSBRIhucB_3M0ZVzFs5IQ8tph4-Rtpg?e=8qutWY',
+        },
+    ];
+
+    return (
+        <div style={styles.appContainer}>
+            <h1 style={styles.header}>Everything Capstone</h1>
+            <div style={styles.container}>
+                {links.map((link, index) => (
+                    <a
+                        key={index}
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={styles.link}
+                    >
+                        {link.title}
+                    </a>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+const styles = {
+    appContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '40px',
+        fontFamily: 'Arial, sans-serif',
+    },
+    header: {
+        marginBottom: '30px',
+        fontSize: '48px',
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '20px',
+    },
+    link: {
+        textDecoration: 'none',
+        padding: '20px 20px',
+        backgroundColor: '#4CAF50',
+        color: 'white',
+        borderRadius: '5px',
+        fontSize: '32px',
+        textAlign: 'center',
+        width: '400px',
+    },
+};
